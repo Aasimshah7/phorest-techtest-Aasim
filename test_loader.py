78b271fde8a721da1e5fb4388b67a93b87762565
aasimshah.2000 <aasimshah.2000@gmail.com>
2025-03-27 00:21:08 +0000

    Modified test script to verify loyalty analyzer

--- test_loader.py
+++ test_loader.py
@@ -1,7 +1,12 @@
 from src.database import Database
 from src.data_loader import DataLoader
+from src.loyalty import LoyaltyAnalyzer
 
 db = Database()
 loader = DataLoader(db, "data")
 loader.load_all_data()
-print("Data loaded successfully!")
+
+analyzer = LoyaltyAnalyzer(db)
+top_clients = analyzer.get_top_clients(50, "2018-01-01")  # From problem spec
+for client in top_clients:
+    print(f"{client['first_name']} {client['last_name']} ({client['email']}): {client['points']} points")
